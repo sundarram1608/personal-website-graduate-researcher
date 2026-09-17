32847401fd519dd27082e846d5a067dd038df855
Sundar Ram Subramanian <110481773+sundarram1608@users.noreply.github.com>
2026-09-17 14:40:19 -0700

    Update CV link to Google Drive folder

--- eportfolio.py
+++ eportfolio.py
@@ -1604,7 +1604,7 @@ st.markdown(
                         Email
                     </a>
                     <!-- View CV -->
-                    <a href="https://drive.google.com/file/d/1Q0EDuJfznRASAvadAjP-wG2ZmDwH324u/view?usp=share_link"
+                    <a href="https://drive.google.com/drive/folders/1pemjNPQ4p-cu2FzKG6EgwiHMIDjguOkM?usp=share_link"
                        target="_blank"
                        rel="noopener noreferrer"
                        class="cta-button-resume">
